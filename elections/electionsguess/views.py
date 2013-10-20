230f998bb720f5ec01a98e5b4c5e0ca05d66976f
Purvi Gupta <purvi24@users.noreply.github.com>
2013-10-20 13:17:52 +0530

    views.py testing

--- elections/electionsguess/views.py
+++ elections/electionsguess/views.py
@@ -2,8 +2,15 @@ from django.http import HttpResponse
 
 
 def search_constituencies(request,place):
-
+  #search_form = loader.get_template('fillupform.html')
+  try:
+    return direct_to_template(request, template='fillupform.html' %place)
+  except TemplateDoesNotExist:
+    raise Http404()
   
 
-def home_page(request){
- 
+def home_page(request, page){
+  try:
+    return direct_to_template(request, template='homepage.html' %page)
+  except TemplateDoesNotExist:
+    raise Http404()
