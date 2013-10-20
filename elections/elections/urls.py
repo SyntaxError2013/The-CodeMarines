4ceb40661eb94791b26d3b910d5a85ec96baf4e0
ayush <ayushtiwariiitr@gmail.com>
2013-10-20 13:05:34 +0530

    urls changed

--- elections/elections/urls.py
+++ elections/elections/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'elections.views.home_page', name='index.html'),
+    url(r'^$', 'electionsguess.views.home_page', name='index.html'),
     # url(r'^elections/', include('elections.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
