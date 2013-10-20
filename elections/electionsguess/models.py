5affd2c353dfa7e17a2bc154afb34c7f77069c6f
ayush <ayushtiwariiitr@gmail.com>
2013-10-20 11:44:38 +0530

    changes to settings

--- elections/electionsguess/models.py
+++ elections/electionsguess/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class election_data(models.Model):
+      consti_name=models.CharField(max_length=200)
+      last_winner=models.CharField(max_length=100)
+      pre_winner=models.CharField(max_length=100)
+      major_caste=models.CharField(max_length=50)
