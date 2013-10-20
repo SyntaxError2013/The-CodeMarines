b486903d92e8ee618d12542697f8145c8e807078
Manohar-94 <khmanohar3@gmail.com>
2013-10-20 13:21:06 +0530

    Update models.py

--- elections/electionsguess/models.py
+++ elections/electionsguess/models.py
@@ -5,3 +5,9 @@ class election_data(models.Model):
       last_winner=models.CharField(max_length=100)
       pre_winner=models.CharField(max_length=100)
       major_caste=models.CharField(max_length=50)
+      
+class candidate_data(models.Model):
+      electiondata=models.ForeignKey(election_data)
+      party=models.CharField(max_length=50)
+      cast_support=models.CharField(max_length=50)
+      campaigns=models.CharField(max_length=10)
